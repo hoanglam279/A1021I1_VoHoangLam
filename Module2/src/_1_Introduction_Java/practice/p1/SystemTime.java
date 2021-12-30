89418fff4cd19fb45886bc1df142743edd337846
hoanglam279 <hoanglam92h1.com>
2021-12-30 16:12:50 +0700

    test

---
+++ Module2/src/_1_Introduction_Java/practice/p1/SystemTime.java
@@ -0,0 +1,11 @@
+package _1_Introduction_Java.practice.p1;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
