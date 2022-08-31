46a1da4da5bad723485209854dae3c47aae5362c
hoanglam279 <hoanglam92h1@gmail.com>
2022-08-31 16:08:00 +0700

    update

---
+++ Module_5/ss5_Components_Interaction/exercise/angular-rating-bar/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-rating-bar';
+  point = 0;
+  showPoint(event: number) {
+    this.point = event;
+  }
+}
